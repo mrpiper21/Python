dea62631945e2e2554b9c66180c3aa36cc7e8d70
Bernard <benbaah104@gmail.com>
2025-04-12 23:09:50 +0000

    I am jacked up

---
+++ date.py
@@ -0,0 +1,8 @@
+import datetime
+
+x = datetime.datetime.now()
+print(x)
+print(x.strftime("%A"))
+
+y = datetime.datetime(2020, 5, 17)
+print(y)
---
+++ jsn.py
@@ -0,0 +1,13 @@
+import json
+
+# some JSON:
+x =  '{ "name":"John", "age":30, "city":"New York"}'
+
+# parse x:
+y = json.loads(x)
+
+# the result is a Python dictionary:
+print(y["age"])
+
+y = json.dumps(x)
+print(y)
